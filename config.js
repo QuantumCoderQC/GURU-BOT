5d16ec8e674f8ff54e35c58fb88f814ff1845a6a
QuantumCoderQC <55564981+QuantumCoderQC@users.noreply.github.com>
2023-11-27 16:08:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919242918191', 'BALAJI', true], 
   [''] 
 ] //Number of owners
 
